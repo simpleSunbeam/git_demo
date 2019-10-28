2f60807483e8e7274cf44c7f8ca2136b388a9211
wk <1017064666@qq.com>
2019-10-28 22:46:52 +0800

    git test

--- src/main/java/com/wk/git_demo/GitDemoApplication.java
+++ src/main/java/com/wk/git_demo/GitDemoApplication.java
@@ -8,6 +8,8 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
+
+        System.out.println("hello git");
     }
 
 }
